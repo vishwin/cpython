16dd43a54fda85380d828257682b1663fe99da3f
Charlie Li <vishwin@FreeBSD.org>
2023-02-09 00:19:28 -0500

    [3.8] Do not write bytecode when running as root

--- Python/initconfig.c
+++ Python/initconfig.c
@@ -22,6 +22,9 @@
 #    include <fcntl.h>    /* O_BINARY */
 #  endif
 #endif
+#ifdef HAVE_UNISTD_H
+#  include <unistd.h>             // geteuid()
+#endif
 
 
 /* --- Command line options --------------------------------------- */
@@ -664,6 +667,11 @@ config_init_defaults(PyConfig *config)
     config->optimization_level = 0;
     config->parser_debug= 0;
     config->write_bytecode = 1;
+#ifdef HAVE_UNISTD_H
+    if (geteuid() == 0) {
+        config->write_bytecode = 0;
+    }
+#endif
     config->verbose = 0;
     config->quiet = 0;
     config->user_site_directory = 1;
