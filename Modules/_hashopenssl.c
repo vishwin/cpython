b4089dcc7aa41579b784d71dce26f05008fad3c2
Charlie Li <vishwin@FreeBSD.org>
2023-03-26 20:06:58 -0400

    [3.13] cherry-pick hashopenssl.c workarounds from 3.10

--- Modules/_hashopenssl.c
+++ Modules/_hashopenssl.c
@@ -45,7 +45,9 @@
 
 #define MUNCH_SIZE INT_MAX
 
+#ifndef LIBRESSL_VERSION_NUMBER
 #define PY_OPENSSL_HAS_SCRYPT 1
+#endif
 #if defined(NID_sha3_224) && defined(NID_sha3_256) && defined(NID_sha3_384) && defined(NID_sha3_512)
 #define PY_OPENSSL_HAS_SHA3 1
 #endif
@@ -971,9 +973,12 @@ _hashlib_HASH(PyObject *module, const char *digestname, PyObject *data_obj,
         goto exit;
     }
 
+#ifdef PY_OPENSSL_HAS_SHAKE
     if ((EVP_MD_flags(digest) & EVP_MD_FLAG_XOF) == EVP_MD_FLAG_XOF) {
         type = get_hashlib_state(module)->EVPXOFtype;
-    } else {
+    } else
+#endif
+    {
         type = get_hashlib_state(module)->EVPtype;
     }
 
