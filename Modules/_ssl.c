66b412d114a671baafd6a2e6bc2db0f2e20d4c1e
Charlie Li <vishwin@FreeBSD.org>
2024-04-26 19:36:18 -0400

    [3.12] LibreSSL has x509_object_dup

--- Modules/_ssl.c
+++ Modules/_ssl.c
@@ -4485,7 +4485,7 @@ set_sni_callback(PySSLContext *self, PyObject *arg, void *c)
     return 0;
 }
 
-#if OPENSSL_VERSION_NUMBER < 0x30300000L
+#if OPENSSL_VERSION_NUMBER < 0x30300000L && !defined(LIBRESSL_VERSION_NUMBER)
 static X509_OBJECT *x509_object_dup(const X509_OBJECT *obj)
 {
     int ok;
